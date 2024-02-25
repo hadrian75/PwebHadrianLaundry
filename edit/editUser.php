e0ce6a8656735ce2a7179000be20dfacb398d750
Drian <igedehadrian75@gmail.com>
2024-02-25 16:21:59 +0800

    Nama username

--- edit/editUser.php
+++ edit/editUser.php
@@ -20,11 +20,11 @@ $data = mysqli_fetch_assoc($query);
             <div class="mb-5">
             <div class="mb-5">
     <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama User</label>
-    <input type="text" name="nama" id="nama"  class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 p-2" placeholder="Isi Alamat Outlet" value="<?=$data["nama_paket"]?>">
+    <input type="text" name="nama" id="nama"  class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 p-2" placeholder="Nama User" value="<?=$data["nama"]?>">
   </div>
   <div class="mb-5">
     <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-    <input type="text" name="username" id="username"  class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 p-2" placeholder="Isi Alamat Outlet" value="<?=$data["nama_paket"]?>">
+    <input type="text" name="username" id="username"  class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 p-2" placeholder="Username User" value="<?=$data["username"]?>">
   </div>
     <label for="idOutlet" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Outlet</label>
     <select name="idOutlet" id="idOutlet" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
