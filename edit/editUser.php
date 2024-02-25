1b2d6de673d99f5c8cd2f6b84621b179a1209228
Drian <igedehadrian75@gmail.com>
2024-02-25 16:33:29 +0800

    Pass Lama Baru

--- edit/editUser.php
+++ edit/editUser.php
@@ -49,11 +49,11 @@ $data = mysqli_fetch_assoc($query);
   
 
   <div class="mb-5">
-    <label for="pass" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+    <label for="pass" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password Lama</label>
     <input type="password" name="passConfirm" id="pass"  class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 p-2" placeholder="Isi Password Lama">
   </div>
   <div class="mb-5">
-    <label for="pass" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+    <label for="pass" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password Baru</label>
     <input type="password" name="pass" id="pass"  class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 p-2" placeholder="Isi Password Baru">
   </div>
 
